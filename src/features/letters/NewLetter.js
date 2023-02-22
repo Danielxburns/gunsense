62b06a78c52c70f62d877ac53ffa6a39fa189be6
Danielxburns <danielburnstech@gmail.com>
2023-02-21 19:10:22 -0600

    create generic  NewLetter form data and pass it to PostNewLetter() onSubmit

--- src/features/letters/NewLetter.js
+++ src/features/letters/NewLetter.js
@@ -1,9 +1,10 @@
 import { forwardRef } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { postNewLetter } from './lettersSlice'
+
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
+//import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Dialog from '@mui/material/Dialog';
@@ -21,7 +22,7 @@ const Transition = forwardRef(function Transition(props, ref) {
 
 export function NewLetter({ open, handleClose, rep }) {
   const dispatch = useDispatch();
-
+  const user = useSelector((state)=>state.users.currentUser)
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -31,6 +32,7 @@ export function NewLetter({ open, handleClose, rep }) {
     dispatch(postNewLetter(dataObj));
     handleClose();
   };
+
   return(
     <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
     <Container maxWidth='lg'>
@@ -41,22 +43,24 @@ export function NewLetter({ open, handleClose, rep }) {
     </DialogActions>
       <CssBaseline />
       <DialogTitle variant="h5" sx={{ textAlign: 'center' }}>
-        <div>
+        {rep && <div>
           Letter to {rep.title} {rep.name}
-        </div>
+        </div>}
       </DialogTitle>
       <DialogContent>
         <Box component="form" onSubmit={handleSubmit}
-        sx={{ my: 3, px: 4 }}>
-          <Typography>From: username</Typography>
-          <Typography>Date:</Typography>
-          <Typography>this is a new letter</Typography>
+        sx={{ my: 3, px: 4, display: 'flex', flexDirection: 'column' }}>
+          <div>From: <TextField required variant="standard" size="small" name="sender" defaultValue={user ? user.email : 'user not logged in'} /></div>
+          <div>To: <TextField required variant="standard" size="small" name="recipient" defaultValue={rep ? `${rep.name}` : 'none selected'} /></div>
+          <div>Date: <TextField required variant="standard" size="small" name="date" defaultValue={new Date().toLocaleDateString()} /> </div>
+          <TextField required multiline rows={5} sx={{ mt:3 }}
+          name="body" label="Body" id="body" defaultValue={'Dear Government Official, \n'}></TextField>
           <Button
           type="submit"
           variant="contained"
-          sx={{ mt: 3, mb: 2 }}
+          sx={{ mt: 3, mb: 2, maxWidth: 100 }}
         >
-          Send letter
+          Send
         </Button>
         </Box>
       </DialogContent>
